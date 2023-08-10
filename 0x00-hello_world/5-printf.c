34c48bee7e43296f583b8c37707e9dbfcdab6775
Lamin Jatta <seniorjatta@gmail.com>
2023-08-10 08:35:06 -0700

    checking for bytes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - this is a method that include all my codes 
+ * main - this is the master method
  *
- * Return: 0 when every thing goes well
+ * Return: 0 this is ther value
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
-}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+} 
+
