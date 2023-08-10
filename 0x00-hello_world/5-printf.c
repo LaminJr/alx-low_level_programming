c860681fe5c37025d4d8b1602100dc10daba340e
Lamin Jatta <seniorjatta@gmail.com>
2023-08-10 09:03:07 -0700

    print with f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - this is the master method
+ * main  - print "with proper grammar, but the outcome is a piece of art, " followed newline
+ * Return: Always 0.
  *
- * Return: 0 this is ther value
  */
 
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-} 
 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
