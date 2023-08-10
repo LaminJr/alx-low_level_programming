40989507ce7ef50d1a129e9b3f520d943657ddac
Lamin Jatta <seniorjatta@gmail.com>
2023-08-10 06:26:23 -0700

    the printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is a method that include all my codes 
+ *
+ * Return: 0 when every thing goes well
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
