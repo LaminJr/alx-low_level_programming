a7e63114ba961841d3091ff05fdbdf245908ec96
Lamin Jatta <seniorjatta@gmail.com>
2023-08-12 04:59:24 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char);
 	printf("Size of an int: %lu byte(0)\n", sizeof(int);
+	printf("Size of a long int: %lu byte(0)\n", sizeof(long int);
+	printf("Size of a long long int: %lu byte(0)\n", sizeof(long long int);
+	printf("Size of a float: %lu byte(0)\n", sizeof(float);
 	return (0);
 }
