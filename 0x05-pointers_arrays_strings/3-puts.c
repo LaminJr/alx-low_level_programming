d67171e0347ee9a16333ddb038f3da7e063e9906
Lamin Jatta <seniorjatta@gmail.com>
2023-08-22 00:35:37 -0700

    printing string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function
+ * @str: parameter
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[1] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
