776366968e23977362a104ddee16be85f567cbfd
Lamin Jatta <seniorjatta@gmail.com>
2023-08-25 17:46:30 -0700

    converting to lower case use -32

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - function
+ * @s: string to convert
+ * Return: void
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
